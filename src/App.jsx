316e6ff11171f7efa91e7f63169f0058142ae12a
Marina Majdenić <marina.majden@gmail.com>
2024-05-01 21:33:18 +0200

    set up project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1>hello</h1>
+      </div>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus
+        exercitationem, in nemo aperiam voluptatibus, velit sit eum consectetur,
+        aspernatur deserunt nam assumenda ipsa aliquam. Iste nulla quidem amet
+        laborum excepturi.
+      </p>
+    </>
+  );
+}
+
+export default App;
